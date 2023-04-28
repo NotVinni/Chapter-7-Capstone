b04ff67fb5aae08f9c00fb870e20785408c137cf
VINNI VONGNARAJ <VVongnar7581@slps.org>
2023-04-28 09:18:50 -0500

    Hello

--- Chapter 7 Capstone/Main Form.Designer.vb
+++ Chapter 7 Capstone/Main Form.Designer.vb
@@ -23,14 +23,19 @@ Partial Class frmMain
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmMain))
         Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
         Me.btnPlay = New System.Windows.Forms.Button()
-        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lblWheel = New System.Windows.Forms.Label()
         Me.ListBox1 = New System.Windows.Forms.ListBox()
-        Me.Label2 = New System.Windows.Forms.Label()
-        Me.Label3 = New System.Windows.Forms.Label()
+        Me.lblWheel2 = New System.Windows.Forms.Label()
+        Me.lblWheel3 = New System.Windows.Forms.Label()
         Me.btnExit = New System.Windows.Forms.Button()
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.lblDescTeam1 = New System.Windows.Forms.Label()
+        Me.lblTeam1 = New System.Windows.Forms.Label()
+        Me.btnAddTeam1 = New System.Windows.Forms.Button()
+        Me.btnSubTeam1 = New System.Windows.Forms.Button()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
@@ -39,59 +44,56 @@ Partial Class frmMain
         '
         'btnPlay
         '
-        Me.btnPlay.Location = New System.Drawing.Point(237, 20)
+        Me.btnPlay.Location = New System.Drawing.Point(309, 12)
         Me.btnPlay.Name = "btnPlay"
-        Me.btnPlay.Size = New System.Drawing.Size(107, 36)
+        Me.btnPlay.Size = New System.Drawing.Size(112, 40)
         Me.btnPlay.TabIndex = 0
         Me.btnPlay.Text = "&Play"
         Me.btnPlay.UseVisualStyleBackColor = True
         '
-        'Label1
+        'lblWheel
         '
-        Me.Label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
-        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(12, 12)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(120, 40)
-        Me.Label1.TabIndex = 1
-        Me.Label1.Text = "Label1"
-        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.lblWheel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblWheel.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblWheel.Location = New System.Drawing.Point(12, 12)
+        Me.lblWheel.Name = "lblWheel"
+        Me.lblWheel.Size = New System.Drawing.Size(145, 40)
+        Me.lblWheel.TabIndex = 1
+        Me.lblWheel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
         '
         'ListBox1
         '
         Me.ListBox1.FormattingEnabled = True
-        Me.ListBox1.Items.AddRange(New Object() {"1,000", "Skip", "Bankrupt", "5,000", "2,000", "10,000", "Skip", "2,000", "2,000", "1,000", "1,000", "Bankrupt", "5,000", "2,000", "1,000", "1,000", "1,000", "Skip", "5,000", "1,000", "1,000", "2,000", "1,000", "1,000", "2,000"})
-        Me.ListBox1.Location = New System.Drawing.Point(12, 281)
+        Me.ListBox1.Items.AddRange(New Object() {"1,000", "5,000", "2,000", "10,000", "Skip", "2,000", "2,000", "1,000", "1,000", "Bankrupt", "5,000", "2,000", "Skip", "1,000", "1,000", "1,000", "Skip", "5,000", "1,000", "1,000", "Bankrupt", "2,000", "1,000", "1,000", "2,000"})
+        Me.ListBox1.Location = New System.Drawing.Point(225, 98)
         Me.ListBox1.Name = "ListBox1"
         Me.ListBox1.Size = New System.Drawing.Size(134, 147)
         Me.ListBox1.TabIndex = 5
         Me.ListBox1.Visible = False
         '
-        'Label2
+        'lblWheel2
         '
-        Me.Label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
-        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label2.Location = New System.Drawing.Point(12, 52)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(120, 40)
-        Me.Label2.TabIndex = 6
-        Me.Label2.Text = "Label5"
-        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.lblWheel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblWheel2.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblWheel2.Location = New System.Drawing.Point(12, 52)
+        Me.lblWheel2.Name = "lblWheel2"
+        Me.lblWheel2.Size = New System.Drawing.Size(145, 40)
+        Me.lblWheel2.TabIndex = 6
+        Me.lblWheel2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
         '
-        'Label3
+        'lblWheel3
         '
-        Me.Label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
-        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label3.Location = New System.Drawing.Point(12, 92)
-        Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(120, 40)
-        Me.Label3.TabIndex = 7
-        Me.Label3.Text = "Label5"
-        Me.Label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.lblWheel3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblWheel3.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblWheel3.Location = New System.Drawing.Point(12, 92)
+        Me.lblWheel3.Name = "lblWheel3"
+        Me.lblWheel3.Size = New System.Drawing.Size(145, 40)
+        Me.lblWheel3.TabIndex = 7
+        Me.lblWheel3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
         '
         'btnExit
         '
-        Me.btnExit.Location = New System.Drawing.Point(237, 92)
+        Me.btnExit.Location = New System.Drawing.Point(443, 228)
         Me.btnExit.Name = "btnExit"
         Me.btnExit.Size = New System.Drawing.Size(107, 36)
         Me.btnExit.TabIndex = 8
@@ -100,27 +102,74 @@ Partial Class frmMain
         '
         'PictureBox1
         '
-        Me.PictureBox1.Location = New System.Drawing.Point(138, 49)
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(163, 52)
         Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(93, 40)
+        Me.PictureBox1.Size = New System.Drawing.Size(109, 40)
         Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
         Me.PictureBox1.TabIndex = 9
         Me.PictureBox1.TabStop = False
+        '
+        'lblDescTeam1
+        '
+        Me.lblDescTeam1.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblDescTeam1.Location = New System.Drawing.Point(12, 153)
+        Me.lblDescTeam1.Name = "lblDescTeam1"
+        Me.lblDescTeam1.Size = New System.Drawing.Size(145, 40)
+        Me.lblDescTeam1.TabIndex = 10
+        Me.lblDescTeam1.Text = "Team 1:"
+        Me.lblDescTeam1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblTeam1
+        '
+        Me.lblTeam1.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTeam1.Location = New System.Drawing.Point(12, 191)
+        Me.lblTeam1.Name = "lblTeam1"
+        Me.lblTeam1.Size = New System.Drawing.Size(145, 40)
+        Me.lblTeam1.TabIndex = 11
+        Me.lblTeam1.Text = "$0"
+        Me.lblTeam1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'btnAddTeam1
+        '
+        Me.btnAddTeam1.Enabled = False
+        Me.btnAddTeam1.Font = New System.Drawing.Font("Microsoft Sans Serif", 21.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnAddTeam1.Location = New System.Drawing.Point(18, 234)
+        Me.btnAddTeam1.Name = "btnAddTeam1"
+        Me.btnAddTeam1.Size = New System.Drawing.Size(71, 36)
+        Me.btnAddTeam1.TabIndex = 12
+        Me.btnAddTeam1.Text = "+"
+        Me.btnAddTeam1.UseVisualStyleBackColor = True
+        '
+        'btnSubTeam1
+        '
+        Me.btnSubTeam1.Enabled = False
+        Me.btnSubTeam1.Font = New System.Drawing.Font("Microsoft Sans Serif", 21.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSubTeam1.Location = New System.Drawing.Point(95, 234)
+        Me.btnSubTeam1.Name = "btnSubTeam1"
+        Me.btnSubTeam1.Size = New System.Drawing.Size(71, 36)
+        Me.btnSubTeam1.TabIndex = 13
+        Me.btnSubTeam1.Text = "-"
+        Me.btnSubTeam1.UseVisualStyleBackColor = True
         '
         'frmMain
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.AutoSize = True
-        Me.ClientSize = New System.Drawing.Size(384, 162)
+        Me.ClientSize = New System.Drawing.Size(600, 307)
         Me.ControlBox = False
+        Me.Controls.Add(Me.btnSubTeam1)
+        Me.Controls.Add(Me.btnAddTeam1)
+        Me.Controls.Add(Me.lblTeam1)
+        Me.Controls.Add(Me.lblDescTeam1)
         Me.Controls.Add(Me.PictureBox1)
         Me.Controls.Add(Me.btnExit)
-        Me.Controls.Add(Me.Label3)
-        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.lblWheel3)
+        Me.Controls.Add(Me.lblWheel2)
         Me.Controls.Add(Me.ListBox1)
-        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.lblWheel)
         Me.Controls.Add(Me.btnPlay)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
         Me.Name = "frmMain"
         Me.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
@@ -132,10 +181,14 @@ Partial Class frmMain
 
     Friend WithEvents Timer1 As Timer
     Friend WithEvents btnPlay As Button
-    Friend WithEvents Label1 As Label
+    Friend WithEvents lblWheel As Label
     Friend WithEvents ListBox1 As ListBox
-    Friend WithEvents Label2 As Label
-    Friend WithEvents Label3 As Label
+    Friend WithEvents lblWheel2 As Label
+    Friend WithEvents lblWheel3 As Label
     Friend WithEvents btnExit As Button
     Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents lblDescTeam1 As Label
+    Friend WithEvents lblTeam1 As Label
+    Friend WithEvents btnAddTeam1 As Button
+    Friend WithEvents btnSubTeam1 As Button
 End Class
--- Chapter 7 Capstone/Main Form.vb
+++ Chapter 7 Capstone/Main Form.vb
@@ -5,60 +5,103 @@
 Option Strict On
 Option Explicit On
 Option Infer Off
+
+Imports System.Runtime.CompilerServices
+
 Public Class frmMain
+
+    ' Class level variables
     Dim Timer As Integer = 0
     Dim rand As New Random
     Dim word As Integer
     Dim word2 As Integer
     Dim word3 As Integer
-
+    Dim intWheel As Integer
+    Dim intTeam1 As Integer = 0
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnPlay.Click
+        ' Button starts the timer which ends itself after a set time
         Timer1.Enabled = True
-
-    End Sub
-
-    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
-
+        btnAddTeam1.Enabled = False
+        btnSubTeam1.Enabled = False
     End Sub
 
     Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        ' Variables to help create a scrolling effects
+        Dim int0 As Integer = 0
+        Dim int1 As Integer = 1
+        Dim int2 As Integer = 2
+
         word = rand.Next(0, 24)
         word2 = rand.Next(0, 24)
         word3 = rand.Next(0, 24)
-        Dim Test As Integer = 0
-        Dim Test2 As Integer = 1
-        Dim Test3 As Integer = 2
-        Dim i As Integer = ListBox1.Items.Count
+
         Timer += 1
 
+        ' Loop to change the labels color and create a scrolling effect which ends on a random number
+        ' Bankrupt method TBA, likely will check if lblwheel2 contains bankrupt and make a button to bankrupt visible 
         For T As Integer = 0 To 0
             If Timer <= 17 Then
-                Label1.Text = CStr(ListBox1.Items(Test + Timer))
-                Label2.Text = CStr(ListBox1.Items(Test2 + Timer))
-                Label3.Text = CStr(ListBox1.Items(Test3 + Timer))
-                Label3.BackColor = Color.FromArgb(rand.Next(0, 256), rand.Next(0, 256), rand.Next(0, 256))
-                Label1.BackColor = Color.FromArgb(rand.Next(0, 256), rand.Next(0, 256), rand.Next(0, 256))
-                Label2.BackColor = Color.FromArgb(rand.Next(0, 256), rand.Next(0, 256), rand.Next(0, 256))
+                lblWheel.Text = CStr(ListBox1.Items(int0 + Timer))
+                lblWheel2.Text = CStr(ListBox1.Items(int1 + Timer))
+                lblWheel3.Text = CStr(ListBox1.Items(int2 + Timer))
+
+                ' If statement that adds a $ symbol, want to create a function of some sort for this later maybe
+                If lblWheel.Text Like "?,000" Or lblWheel.Text Like "??,000" Then
+                    lblWheel.Text = lblWheel.Text.Insert(0, "$")
+                End If
+                If lblWheel2.Text Like "?,000" Or lblWheel2.Text Like "??,000" Then
+                    lblWheel2.Text = lblWheel2.Text.Insert(0, "$")
+                End If
+                If lblWheel3.Text Like "?,000" Or lblWheel3.Text Like "??,000" Then
+                    lblWheel3.Text = lblWheel3.Text.Insert(0, "$")
+                End If
+
+                lblWheel.BackColor = Color.FromArgb(rand.Next(0, 256), rand.Next(0, 256), rand.Next(0, 256))
+                lblWheel2.BackColor = Color.FromArgb(rand.Next(0, 256), rand.Next(0, 256), rand.Next(0, 256))
+                lblWheel3.BackColor = Color.FromArgb(rand.Next(0, 256), rand.Next(0, 256), rand.Next(0, 256))
                 Timer1.Interval = 150
+
             Else
-                Label3.Text = CStr(ListBox1.Items(word))
-                Label1.Text = CStr(ListBox1.Items(word2))
-                Label2.Text = CStr(ListBox1.Items(word3))
+                lblWheel.Text = CStr(ListBox1.Items(word))
+                lblWheel2.Text = CStr(ListBox1.Items(word2))
+                lblWheel3.Text = CStr(ListBox1.Items(word3))
+
+                ' See line 44
+                If lblWheel.Text Like "?,000" Or lblWheel.Text Like "??,000" Then
+                    lblWheel.Text = lblWheel.Text.Insert(0, "$")
+                End If
+                If lblWheel2.Text Like "?,000" Or lblWheel2.Text Like "??,000" Then
+                    lblWheel2.Text = lblWheel2.Text.Insert(0, "$")
+                End If
+                If lblWheel3.Text Like "?,000" Or lblWheel3.Text Like "??,000" Then
+                    lblWheel3.Text = lblWheel3.Text.Insert(0, "$")
+                End If
+
 
                 Timer = 0
+                btnAddTeam1.Enabled = True
+                btnSubTeam1.Enabled = True
                 Timer1.Enabled = False
                 Exit For
             End If
         Next T
-
-
     End Sub
 
     Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
         Me.Close()
     End Sub
+    Dim intTotal As Integer
 
-    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+    Private Sub btnAddTeam1_Click(sender As Object, e As EventArgs) Handles btnAddTeam1.Click
+        ' Button to add money to teams/person. Likely will be done by grabbing values from the listbox unless i figure out how to grab it from the label
+
+        If lblWheel2.Text Like "??,000" Or lblWheel2.Text Like "???,000" Then
+            Dim intValue As Integer = CInt(ListBox1.Items(word2))
+            intTotal += intValue
+            lblTeam1.Text = intTotal.ToString("C0")
+        Else
+            MsgBox("Please select a different option.", 0, "Error")
+        End If
 
     End Sub
 End Class
