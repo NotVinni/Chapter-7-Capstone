c2e95f843d28db68528f8be9366c99151af59e02
VINNI VONGNARAJ <VVongnar7581@slps.org>
2023-05-10 12:41:16 -0500

    Most Updated

--- Chapter 7 Capstone/Main Form.Designer.vb
+++ Chapter 7 Capstone/Main Form.Designer.vb
@@ -32,14 +32,25 @@ Partial Class frmMain
         Me.lblWheel3 = New System.Windows.Forms.Label()
         Me.btnExit = New System.Windows.Forms.Button()
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
-        Me.lblDescTeam1 = New System.Windows.Forms.Label()
-        Me.lblTeam1 = New System.Windows.Forms.Label()
-        Me.btnAddTeam1 = New System.Windows.Forms.Button()
-        Me.btnBankrupt1 = New System.Windows.Forms.Button()
-        Me.btnStart = New System.Windows.Forms.Button()
+        Me.lblDescPlay1 = New System.Windows.Forms.Label()
+        Me.lblPlay1 = New System.Windows.Forms.Label()
+        Me.btnAddPlay1 = New System.Windows.Forms.Button()
+        Me.btnBankruptPlay1 = New System.Windows.Forms.Button()
+        Me.btnGuess = New System.Windows.Forms.Button()
         Me.lstPhrases = New System.Windows.Forms.ListBox()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.MenuStrip = New System.Windows.Forms.MenuStrip()
+        Me.ViewToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.menuOpen = New System.Windows.Forms.ToolStripMenuItem()
+        Me.txtGuess = New System.Windows.Forms.TextBox()
+        Me.lblRanWord = New System.Windows.Forms.Label()
+        Me.btnBankruptPlay2 = New System.Windows.Forms.Button()
+        Me.btnAddPlay2 = New System.Windows.Forms.Button()
+        Me.lblPlay2 = New System.Windows.Forms.Label()
+        Me.lblDescPlay2 = New System.Windows.Forms.Label()
+        Me.lblGuessedLetters = New System.Windows.Forms.Label()
+        Me.lblDescGuessedLetters = New System.Windows.Forms.Label()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.MenuStrip.SuspendLayout()
         Me.SuspendLayout()
         '
         'Timer1
@@ -47,7 +58,7 @@ Partial Class frmMain
         '
         'btnPlay
         '
-        Me.btnPlay.Location = New System.Drawing.Point(309, 12)
+        Me.btnPlay.Location = New System.Drawing.Point(305, 26)
         Me.btnPlay.Name = "btnPlay"
         Me.btnPlay.Size = New System.Drawing.Size(112, 40)
         Me.btnPlay.TabIndex = 0
@@ -59,7 +70,7 @@ Partial Class frmMain
         Me.lblWheel.BackColor = System.Drawing.Color.Red
         Me.lblWheel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
         Me.lblWheel.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.lblWheel.Location = New System.Drawing.Point(12, 12)
+        Me.lblWheel.Location = New System.Drawing.Point(8, 26)
         Me.lblWheel.Name = "lblWheel"
         Me.lblWheel.Size = New System.Drawing.Size(145, 40)
         Me.lblWheel.TabIndex = 1
@@ -70,7 +81,7 @@ Partial Class frmMain
         '
         Me.lstMoney.FormattingEnabled = True
         Me.lstMoney.Items.AddRange(New Object() {"1,000", "5,000", "2,000", "10,000", "Skip", "2,000", "2,000", "1,000", "1,000", "Bankrupt", "5,000", "2,000", "Skip", "1,000", "1,000", "1,000", "Skip", "5,000", "1,000", "1,000", "Bankrupt", "2,000", "1,000", "1,000", "2,000"})
-        Me.lstMoney.Location = New System.Drawing.Point(1035, 12)
+        Me.lstMoney.Location = New System.Drawing.Point(1031, 26)
         Me.lstMoney.Name = "lstMoney"
         Me.lstMoney.Size = New System.Drawing.Size(134, 147)
         Me.lstMoney.TabIndex = 5
@@ -81,7 +92,7 @@ Partial Class frmMain
         Me.lblWheel2.BackColor = System.Drawing.Color.Lime
         Me.lblWheel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
         Me.lblWheel2.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.lblWheel2.Location = New System.Drawing.Point(12, 52)
+        Me.lblWheel2.Location = New System.Drawing.Point(8, 66)
         Me.lblWheel2.Name = "lblWheel2"
         Me.lblWheel2.Size = New System.Drawing.Size(145, 40)
         Me.lblWheel2.TabIndex = 6
@@ -93,7 +104,7 @@ Partial Class frmMain
         Me.lblWheel3.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
         Me.lblWheel3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
         Me.lblWheel3.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.lblWheel3.Location = New System.Drawing.Point(12, 92)
+        Me.lblWheel3.Location = New System.Drawing.Point(8, 106)
         Me.lblWheel3.Name = "lblWheel3"
         Me.lblWheel3.Size = New System.Drawing.Size(145, 40)
         Me.lblWheel3.TabIndex = 7
@@ -102,7 +113,7 @@ Partial Class frmMain
         '
         'btnExit
         '
-        Me.btnExit.Location = New System.Drawing.Point(443, 228)
+        Me.btnExit.Location = New System.Drawing.Point(1058, 533)
         Me.btnExit.Name = "btnExit"
         Me.btnExit.Size = New System.Drawing.Size(107, 36)
         Me.btnExit.TabIndex = 8
@@ -112,79 +123,176 @@ Partial Class frmMain
         'PictureBox1
         '
         Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
-        Me.PictureBox1.Location = New System.Drawing.Point(163, 52)
+        Me.PictureBox1.Location = New System.Drawing.Point(159, 66)
         Me.PictureBox1.Name = "PictureBox1"
         Me.PictureBox1.Size = New System.Drawing.Size(109, 40)
         Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
         Me.PictureBox1.TabIndex = 9
         Me.PictureBox1.TabStop = False
         '
-        'lblDescTeam1
+        'lblDescPlay1
         '
-        Me.lblDescTeam1.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.lblDescTeam1.Location = New System.Drawing.Point(12, 153)
-        Me.lblDescTeam1.Name = "lblDescTeam1"
-        Me.lblDescTeam1.Size = New System.Drawing.Size(145, 40)
-        Me.lblDescTeam1.TabIndex = 10
-        Me.lblDescTeam1.Text = "Team 1:"
-        Me.lblDescTeam1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.lblDescPlay1.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblDescPlay1.Location = New System.Drawing.Point(16, 452)
+        Me.lblDescPlay1.Name = "lblDescPlay1"
+        Me.lblDescPlay1.Size = New System.Drawing.Size(145, 40)
+        Me.lblDescPlay1.TabIndex = 10
+        Me.lblDescPlay1.Text = "Player 1:"
+        Me.lblDescPlay1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
         '
-        'lblTeam1
+        'lblPlay1
         '
-        Me.lblTeam1.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.lblTeam1.Location = New System.Drawing.Point(12, 191)
-        Me.lblTeam1.Name = "lblTeam1"
-        Me.lblTeam1.Size = New System.Drawing.Size(145, 40)
-        Me.lblTeam1.TabIndex = 11
-        Me.lblTeam1.Text = "$0"
-        Me.lblTeam1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.lblPlay1.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblPlay1.Location = New System.Drawing.Point(16, 490)
+        Me.lblPlay1.Name = "lblPlay1"
+        Me.lblPlay1.Size = New System.Drawing.Size(145, 40)
+        Me.lblPlay1.TabIndex = 11
+        Me.lblPlay1.Text = "$0"
+        Me.lblPlay1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
         '
-        'btnAddTeam1
+        'btnAddPlay1
         '
-        Me.btnAddTeam1.Enabled = False
-        Me.btnAddTeam1.Font = New System.Drawing.Font("Microsoft Sans Serif", 21.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btnAddTeam1.Location = New System.Drawing.Point(18, 234)
-        Me.btnAddTeam1.Name = "btnAddTeam1"
-        Me.btnAddTeam1.Size = New System.Drawing.Size(71, 36)
-        Me.btnAddTeam1.TabIndex = 12
-        Me.btnAddTeam1.Text = "+"
-        Me.btnAddTeam1.UseVisualStyleBackColor = True
+        Me.btnAddPlay1.Enabled = False
+        Me.btnAddPlay1.Font = New System.Drawing.Font("Microsoft Sans Serif", 21.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnAddPlay1.Location = New System.Drawing.Point(13, 533)
+        Me.btnAddPlay1.Name = "btnAddPlay1"
+        Me.btnAddPlay1.Size = New System.Drawing.Size(71, 36)
+        Me.btnAddPlay1.TabIndex = 12
+        Me.btnAddPlay1.Text = "+"
+        Me.btnAddPlay1.UseVisualStyleBackColor = True
         '
-        'btnBankrupt1
+        'btnBankruptPlay1
         '
-        Me.btnBankrupt1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btnBankrupt1.Location = New System.Drawing.Point(95, 234)
-        Me.btnBankrupt1.Name = "btnBankrupt1"
-        Me.btnBankrupt1.Size = New System.Drawing.Size(71, 36)
-        Me.btnBankrupt1.TabIndex = 14
-        Me.btnBankrupt1.Text = "&Bankrupt"
-        Me.btnBankrupt1.UseVisualStyleBackColor = True
-        Me.btnBankrupt1.Visible = False
+        Me.btnBankruptPlay1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnBankruptPlay1.Location = New System.Drawing.Point(90, 533)
+        Me.btnBankruptPlay1.Name = "btnBankruptPlay1"
+        Me.btnBankruptPlay1.Size = New System.Drawing.Size(71, 36)
+        Me.btnBankruptPlay1.TabIndex = 14
+        Me.btnBankruptPlay1.Text = "&Bankrupt"
+        Me.btnBankruptPlay1.UseVisualStyleBackColor = True
+        Me.btnBankruptPlay1.Visible = False
         '
-        'btnStart
+        'btnGuess
         '
-        Me.btnStart.Location = New System.Drawing.Point(309, 119)
-        Me.btnStart.Name = "btnStart"
-        Me.btnStart.Size = New System.Drawing.Size(112, 40)
-        Me.btnStart.TabIndex = 15
-        Me.btnStart.Text = "&Start Game"
-        Me.btnStart.UseVisualStyleBackColor = True
+        Me.btnGuess.Location = New System.Drawing.Point(305, 133)
+        Me.btnGuess.Name = "btnGuess"
+        Me.btnGuess.Size = New System.Drawing.Size(112, 40)
+        Me.btnGuess.TabIndex = 15
+        Me.btnGuess.Text = "&Guess"
+        Me.btnGuess.UseVisualStyleBackColor = True
         '
         'lstPhrases
         '
         Me.lstPhrases.FormattingEnabled = True
-        Me.lstPhrases.Location = New System.Drawing.Point(896, 19)
+        Me.lstPhrases.Location = New System.Drawing.Point(892, 33)
         Me.lstPhrases.Name = "lstPhrases"
         Me.lstPhrases.Size = New System.Drawing.Size(133, 134)
         Me.lstPhrases.TabIndex = 16
         '
-        'TextBox1
+        'MenuStrip
         '
-        Me.TextBox1.Location = New System.Drawing.Point(686, 19)
-        Me.TextBox1.Multiline = True
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(193, 150)
-        Me.TextBox1.TabIndex = 17
+        Me.MenuStrip.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ViewToolStripMenuItem})
+        Me.MenuStrip.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip.Name = "MenuStrip"
+        Me.MenuStrip.Size = New System.Drawing.Size(1181, 24)
+        Me.MenuStrip.TabIndex = 17
+        Me.MenuStrip.Text = "Open"
+        '
+        'ViewToolStripMenuItem
+        '
+        Me.ViewToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.menuOpen})
+        Me.ViewToolStripMenuItem.Name = "ViewToolStripMenuItem"
+        Me.ViewToolStripMenuItem.Size = New System.Drawing.Size(44, 20)
+        Me.ViewToolStripMenuItem.Text = "View"
+        '
+        'menuOpen
+        '
+        Me.menuOpen.Name = "menuOpen"
+        Me.menuOpen.Size = New System.Drawing.Size(124, 22)
+        Me.menuOpen.Text = "Open File"
+        '
+        'txtGuess
+        '
+        Me.txtGuess.Location = New System.Drawing.Point(507, 133)
+        Me.txtGuess.Name = "txtGuess"
+        Me.txtGuess.Size = New System.Drawing.Size(100, 20)
+        Me.txtGuess.TabIndex = 18
+        '
+        'lblRanWord
+        '
+        Me.lblRanWord.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblRanWord.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblRanWord.Location = New System.Drawing.Point(507, 33)
+        Me.lblRanWord.Name = "lblRanWord"
+        Me.lblRanWord.Size = New System.Drawing.Size(334, 73)
+        Me.lblRanWord.TabIndex = 19
+        Me.lblRanWord.Text = "Word:"
+        Me.lblRanWord.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'btnBankruptPlay2
+        '
+        Me.btnBankruptPlay2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnBankruptPlay2.Location = New System.Drawing.Point(327, 533)
+        Me.btnBankruptPlay2.Name = "btnBankruptPlay2"
+        Me.btnBankruptPlay2.Size = New System.Drawing.Size(71, 36)
+        Me.btnBankruptPlay2.TabIndex = 23
+        Me.btnBankruptPlay2.Text = "&Bankrupt"
+        Me.btnBankruptPlay2.UseVisualStyleBackColor = True
+        Me.btnBankruptPlay2.Visible = False
+        '
+        'btnAddPlay2
+        '
+        Me.btnAddPlay2.Enabled = False
+        Me.btnAddPlay2.Font = New System.Drawing.Font("Microsoft Sans Serif", 21.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnAddPlay2.Location = New System.Drawing.Point(250, 533)
+        Me.btnAddPlay2.Name = "btnAddPlay2"
+        Me.btnAddPlay2.Size = New System.Drawing.Size(71, 36)
+        Me.btnAddPlay2.TabIndex = 22
+        Me.btnAddPlay2.Text = "+"
+        Me.btnAddPlay2.UseVisualStyleBackColor = True
+        '
+        'lblPlay2
+        '
+        Me.lblPlay2.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblPlay2.Location = New System.Drawing.Point(253, 490)
+        Me.lblPlay2.Name = "lblPlay2"
+        Me.lblPlay2.Size = New System.Drawing.Size(145, 40)
+        Me.lblPlay2.TabIndex = 21
+        Me.lblPlay2.Text = "$0"
+        Me.lblPlay2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblDescPlay2
+        '
+        Me.lblDescPlay2.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblDescPlay2.Location = New System.Drawing.Point(253, 452)
+        Me.lblDescPlay2.Name = "lblDescPlay2"
+        Me.lblDescPlay2.Size = New System.Drawing.Size(145, 40)
+        Me.lblDescPlay2.TabIndex = 20
+        Me.lblDescPlay2.Text = "Player 2:"
+        Me.lblDescPlay2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblGuessedLetters
+        '
+        Me.lblGuessedLetters.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblGuessedLetters.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblGuessedLetters.Location = New System.Drawing.Point(691, 490)
+        Me.lblGuessedLetters.Name = "lblGuessedLetters"
+        Me.lblGuessedLetters.Size = New System.Drawing.Size(334, 73)
+        Me.lblGuessedLetters.TabIndex = 24
+        Me.lblGuessedLetters.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblDescGuessedLetters
+        '
+        Me.lblDescGuessedLetters.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblDescGuessedLetters.Location = New System.Drawing.Point(687, 465)
+        Me.lblDescGuessedLetters.Name = "lblDescGuessedLetters"
+        Me.lblDescGuessedLetters.Size = New System.Drawing.Size(135, 25)
+        Me.lblDescGuessedLetters.TabIndex = 25
+        Me.lblDescGuessedLetters.Text = "Letters Guessed:"
+        Me.lblDescGuessedLetters.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'Timer2
+        '
         '
         'frmMain
         '
@@ -192,13 +300,20 @@ Partial Class frmMain
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(1181, 621)
         Me.ControlBox = False
-        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.lblDescGuessedLetters)
+        Me.Controls.Add(Me.lblGuessedLetters)
+        Me.Controls.Add(Me.btnBankruptPlay2)
+        Me.Controls.Add(Me.btnAddPlay2)
+        Me.Controls.Add(Me.lblPlay2)
+        Me.Controls.Add(Me.lblDescPlay2)
+        Me.Controls.Add(Me.lblRanWord)
+        Me.Controls.Add(Me.txtGuess)
         Me.Controls.Add(Me.lstPhrases)
-        Me.Controls.Add(Me.btnStart)
-        Me.Controls.Add(Me.btnBankrupt1)
-        Me.Controls.Add(Me.btnAddTeam1)
-        Me.Controls.Add(Me.lblTeam1)
-        Me.Controls.Add(Me.lblDescTeam1)
+        Me.Controls.Add(Me.btnGuess)
+        Me.Controls.Add(Me.btnBankruptPlay1)
+        Me.Controls.Add(Me.btnAddPlay1)
+        Me.Controls.Add(Me.lblPlay1)
+        Me.Controls.Add(Me.lblDescPlay1)
         Me.Controls.Add(Me.PictureBox1)
         Me.Controls.Add(Me.btnExit)
         Me.Controls.Add(Me.lblWheel3)
@@ -206,12 +321,16 @@ Partial Class frmMain
         Me.Controls.Add(Me.lstMoney)
         Me.Controls.Add(Me.lblWheel)
         Me.Controls.Add(Me.btnPlay)
+        Me.Controls.Add(Me.MenuStrip)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MainMenuStrip = Me.MenuStrip
         Me.Name = "frmMain"
         Me.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = """Wheel"" of Fortune"
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.MenuStrip.ResumeLayout(False)
+        Me.MenuStrip.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -225,11 +344,22 @@ Partial Class frmMain
     Friend WithEvents lblWheel3 As Label
     Friend WithEvents btnExit As Button
     Friend WithEvents PictureBox1 As PictureBox
-    Friend WithEvents lblDescTeam1 As Label
-    Friend WithEvents lblTeam1 As Label
-    Friend WithEvents btnAddTeam1 As Button
-    Friend WithEvents btnBankrupt1 As Button
-    Friend WithEvents btnStart As Button
+    Friend WithEvents lblDescPlay1 As Label
+    Friend WithEvents lblPlay1 As Label
+    Friend WithEvents btnAddPlay1 As Button
+    Friend WithEvents btnBankruptPlay1 As Button
+    Friend WithEvents btnGuess As Button
     Friend WithEvents lstPhrases As ListBox
-    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents MenuStrip As MenuStrip
+    Friend WithEvents ViewToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents menuOpen As ToolStripMenuItem
+    Friend WithEvents txtGuess As TextBox
+    Friend WithEvents lblRanWord As Label
+    Friend WithEvents btnBankruptPlay2 As Button
+    Friend WithEvents btnAddPlay2 As Button
+    Friend WithEvents lblPlay2 As Label
+    Friend WithEvents lblDescPlay2 As Label
+    Friend WithEvents lblGuessedLetters As Label
+    Friend WithEvents lblDescGuessedLetters As Label
+    Friend WithEvents Timer2 As Timer
 End Class
--- Chapter 7 Capstone/Main Form.vb
+++ Chapter 7 Capstone/Main Form.vb
@@ -6,6 +6,10 @@ Option Strict On
 Option Explicit On
 Option Infer Off
 
+Imports System.Globalization
+Imports System.IO
+Imports System.IO.Pipes
+Imports System.Net.Security
 Imports System.Runtime.CompilerServices
 Imports System.Windows.Forms.VisualStyles.VisualStyleElement
 
@@ -13,16 +17,32 @@ Public Class frmMain
 
     ' Class level variables
     Dim Timer As Integer = 0
+    Dim secondTimer As Integer = 0
     Dim rand As New Random
     Dim word As Integer
     Dim word2 As Integer
     Dim word3 As Integer
-    Dim intTotal As Integer
+
+    Dim intTotal1 As Integer
+    Dim intTotal2 As Integer
+
+    Dim ranIndex As Integer
+    Dim ranWord As String
+    Dim tempWord As String
+    Dim outputWord As String
+
+    Dim Test1 As Integer
+    Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        lblRanWord.Text = String.Empty
+
+    End Sub
+
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnPlay.Click
         ' Button starts the timer which ends itself after a set time
         Timer1.Enabled = True
-        btnAddTeam1.Enabled = False
+        btnAddPlay1.Enabled = False
         btnPlay.Enabled = False
+        Test1 += 1
     End Sub
 
     Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
@@ -67,8 +87,6 @@ Public Class frmMain
                 lblWheel2.Text = CStr(lstMoney.Items(word2))
                 lblWheel3.Text = CStr(lstMoney.Items(word3))
 
-
-
                 ' See line 44
                 If lblWheel.Text Like "?,000" Or lblWheel.Text Like "??,000" Then
                     lblWheel.Text = lblWheel.Text.Insert(0, "$")
@@ -80,16 +98,23 @@ Public Class frmMain
                     lblWheel3.Text = lblWheel3.Text.Insert(0, "$")
                 End If
                 If lblWheel2.Text Like "Bankrupt" Then
-                    btnBankrupt1.Visible = True
+                    btnBankruptPlay1.Visible = True
+                End If
+
+                ' Determines who is in play
+                If Test1 / 2 = 1 Then
+                    btnPlay.Enabled = True
+                    btnAddPlay2.Enabled = True
+                Else
+                    btnPlay.Enabled = True
+                    btnAddPlay1.Enabled = True
                 End If
 
 
                 Timer = 0
-                btnPlay.Enabled = True
-                btnAddTeam1.Enabled = True
                 Timer1.Enabled = False
-                Exit For
-            End If
+                    Exit For
+                End If
         Next T
     End Sub
 
@@ -97,33 +122,105 @@ Public Class frmMain
         Me.Close()
     End Sub
 
-    Private Sub btnAddTeam1_Click(sender As Object, e As EventArgs) Handles btnAddTeam1.Click
+
+
+
+
+
+    ' Loads text file of random phrases     
+    Private Sub menuOpen_Click(sender As Object, e As EventArgs) Handles menuOpen.Click
+        ranWord = String.Empty
+        tempWord = String.Empty
+        outputWord = String.Empty
+
+        Dim Open As New OpenFileDialog()
+        Dim myStreamReader As System.IO.StreamReader
+
+        Open.Filter = "Text [*.txt*]|*.txt|All Files [*.*]|*.*"
+        Open.CheckFileExists = True
+        Open.Title = "OpenFile"
+        Open.ShowDialog(Me)
+
+        Try
+            For Each line As String In File.ReadAllLines(Open.FileName)
+                lstPhrases.Items.Add(line)
+            Next
+            Open.OpenFile()
+            myStreamReader = System.IO.File.OpenText(Open.FileName)
+
+            ranIndex = CInt(lstPhrases.Items.Count * Rnd())
+            ranWord = lstPhrases.Items(ranIndex).ToString.Trim.ToUpper
+
+            For Each c As Char In ranWord
+                tempWord += c + " "
+                outputWord += "- "
+            Next
+            lblRanWord.Text = outputWord
+        Catch ex As Exception
+
+            MsgBox(ex.Message, MsgBoxStyle.Information)
+        End Try
+    End Sub
+
+    ' Guess Letters 
+    Private Sub btGuess_Click(sender As Object, e As EventArgs) Handles btnGuess.Click
+        Dim strGuess As String
+        strGuess = txtGuess.Text.Trim.ToUpper()
+
+        If ranWord.Contains(txtGuess.Text.ToUpper) Then
+            For intIndex As Integer = 0 To tempWord.Length - 1
+                If tempWord(intIndex) = txtGuess.Text.ToUpper Then
+                    outputWord = outputWord.Insert(intIndex, txtGuess.Text.ToUpper)
+                    outputWord = outputWord.Remove(intIndex + 1, 1)
+
+                    tempWord = tempWord.Insert(intIndex, " ")
+                    tempWord = tempWord.Remove(intIndex + 1, 1)
+                End If
+            Next
+        End If
+
+        lblRanWord.Text = outputWord
+    End Sub
+    Private Sub btnBankrupt1_Click(sender As Object, e As EventArgs) Handles btnBankruptPlay1.Click
+        ' Resets Money
+        intTotal1 = 0
+        lblPlay1.Text = intTotal1.ToString("C0")
+        btnBankruptPlay1.Visible = False
+    End Sub
+    Private Sub btnBankruptPlay2_Click(sender As Object, e As EventArgs) Handles btnBankruptPlay2.Click
+        intTotal2 = 0
+        lblPlay2.Text = intTotal2.ToString("C0")
+        btnBankruptPlay1.Visible = False
+    End Sub
+
+    Private Sub btnAddTeam1_Click(sender As Object, e As EventArgs) Handles btnAddPlay1.Click
         ' Button to add money to teams/person. Likely will be done by grabbing values from the listbox unless i figure out how to grab it from the label
         ' Will maybe try to turn this into a function
 
         If lblWheel2.Text Like "??,000" Or lblWheel2.Text Like "???,000" Then
             Dim intValue As Integer = CInt(lstMoney.Items(word2))
-            intTotal += intValue
-            lblTeam1.Text = intTotal.ToString("C0")
-            btnAddTeam1.Enabled = False
+            intTotal1 += intValue
+            lblPlay1.Text = intTotal1.ToString("C0")
+            btnAddPlay1.Enabled = False
         Else
             MsgBox("Please select a different option.", 0, "Error")
-            btnAddTeam1.Enabled = False
+            btnAddPlay1.Enabled = False
         End If
 
     End Sub
 
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles btnAddPlay2.Click
 
-
-    Private Sub btnBankrupt1_Click(sender As Object, e As EventArgs) Handles btnBankrupt1.Click
-        ' Resets Money
-        intTotal = 0
-        lblTeam1.Text = intTotal.ToString("C0")
-        btnBankrupt1.Visible = False
+        If lblWheel2.Text Like "??,000" Or lblWheel2.Text Like "???,000" Then
+            Dim intValue As Integer = CInt(lstMoney.Items(word2))
+            intTotal2 += intValue
+            lblPlay2.Text = intTotal2.ToString("C0")
+            btnAddPlay1.Enabled = False
+        Else
+            MsgBox("Please select a different option.", 0, "Error")
+            btnAddPlay2.Enabled = False
+        End If
     End Sub
 
-    Private Sub btnStart_Click(sender As Object, e As EventArgs) Handles btnStart.Click
-        ' "C:\Users\Vinni\source\repos\Chapter-7-Capstone\Chapter 7 Capstone\phrases.txt"
 
-    End Sub
 End Class
